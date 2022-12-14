6c08c76f973ae720d2b0fefa8395476a9c1c02f5
singhtushar689 <singhtushar689@gmail.com>
2022-12-14 22:18:05 +0530

    Checking Whether Lines are equal or not

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problems");
-            Console.WriteLine("Enter the Co-Ordinates of line");
+            Console.WriteLine("Enter the Co-Ordinates of line1");
             int x1 = int.Parse(Console.ReadLine());
             int x2 = int.Parse(Console.ReadLine());
             int y1 = int.Parse(Console.ReadLine());
@@ -15,10 +15,33 @@ namespace LineComparisonProblem
 
             double length1 = Math.Pow(x2 - x1, 2);
             double length2 = Math.Pow(y2 - y1, 2);
-            double sumOfLine = length1 + length2;
+            double sumOfLine1 = length1 + length2;
 
-            double totalLength = Math.Sqrt(sumOfLine);
-            Console.WriteLine(" Total Length of Line " + totalLength);
+            double totalLength1 = Math.Sqrt(sumOfLine1);
+            Console.WriteLine(" Total Length of Line " + totalLength1);
+
+            Console.WriteLine("Enter the Co-Ordinates of line2");
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int b1 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+
+            double length3 = Math.Pow(a2 - 1, 2);
+            double length4 = Math.Pow(y2 - y1, 2);
+            double sumOfLine2 = length3 + length4;
+
+            double totalLength2 = Math.Sqrt(sumOfLine2);
+            Console.WriteLine(" Total Length of Line " + totalLength2);
+            bool status = totalLength1.Equals(totalLength2);
+            if (status)
+            {
+                Console.WriteLine("Line1 is equal to Line2");
+            }
+            else
+            {
+                Console.WriteLine("Line1 is not equal to Line2");
+            }
         }
+       
     }
 }
