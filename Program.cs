278ba3bd3880271613de8eaec8cca6583514d64b
singhtushar689 <singhtushar689@gmail.com>
2022-12-14 20:42:14 +0530

    Calculate the length of line

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LineComparisonProblem
+﻿using System.ComponentModel;
+
+namespace LineComparisonProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problems");
+            Console.WriteLine("Enter the Co-Ordinates of line");
+            int x1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+
+            double length1 = Math.Pow(x2 - x1, 2);
+            double length2 = Math.Pow(y2 - y1, 2);
+            double sumOfLine = length1 + length2;
+
+            double totalLength = Math.Sqrt(sumOfLine);
+            Console.WriteLine(" Total Length of Line " + totalLength);
         }
     }
 }
